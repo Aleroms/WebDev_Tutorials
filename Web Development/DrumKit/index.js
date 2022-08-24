33cac928887bb8d629b3536e5d1dd549ab3b6063
Aleroms <santiagomorales333@yahoo.com>
2022-08-24 15:41:37 -0700

    added comment to js file

--- Web Development/DrumKit/index.js
+++ Web Development/DrumKit/index.js
@@ -18,42 +18,40 @@ for(var i = 0; i < document.querySelectorAll(".drum").length; i++)
 
 function KeyCheck(key)
 {
+  var audio;
   switch(key)
   {
     case "w":
-      var audio = new Audio("sounds/crash.mp3");
-      audio.play();
+      audio = new Audio("sounds/crash.mp3");
     break;
     case "a":
-      var audio = new Audio("sounds/kick-bass.mp3");
-      audio.play();
+      audio = new Audio("sounds/kick-bass.mp3");
     break;
     case "s":
-      var audio = new Audio("sounds/snare.mp3");
-      audio.play();
+     audio = new Audio("sounds/snare.mp3");
     break;
     case "d":
-      var audio = new Audio("sounds/tom-1.mp3");
-      audio.play();
+     audio = new Audio("sounds/tom-1.mp3");
     break;
     case "j":
-      var audio = new Audio("sounds/tom-2.mp3");
-      audio.play();
+     audio = new Audio("sounds/tom-2.mp3");
     break;
     case "k":
-      var audio = new Audio("sounds/tom-3.mp3");
-      audio.play();
+    audio = new Audio("sounds/tom-3.mp3");
     break;
     case "l":
-      var audio = new Audio("sounds/tom-4.mp3");
-      audio.play();
+    audio = new Audio("sounds/tom-4.mp3");
     break;
-
   }
+  audio.volume = 0.2;
+  audio.play();
+
 }
 
 function buttonAnimation(button)
 {
+  //this function adds and removes the pressed class when
+  //it is pressed or clicked
   var activeButton = document.querySelector("." + button)
   activeButton.classList.add("pressed");
 
